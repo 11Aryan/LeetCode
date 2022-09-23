63817f6de4357e02f46acabdb04b880140e99b52
ARYAN SRIVASTAVA <90818029+11Aryan@users.noreply.github.com>
2022-09-23 21:42:08 +0530

    Time: 38 ms (6.04%), Space: 7.4 MB (92.61%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -10,8 +10,10 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         ListNode *temp=node->next;
-        node->val=temp->val;
-        node->next=temp->next;
+        node->val=temp->val;//agle node ka value copy kar dia isi node mein
+        node->next=temp->next;//agli node ko point na kara ke uske baad wali ko point 
+        delete(temp);
+        
         
     }
 };
