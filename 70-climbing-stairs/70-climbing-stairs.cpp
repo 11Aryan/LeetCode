7211921ebeb0e2569334cbe7ae6cc23767c913f8
ARYAN SRIVASTAVA <90818029+11Aryan@users.noreply.github.com>
2022-09-28 09:10:40 +0530

    Time: 3 ms (36.05%), Space: 6 MB (57.15%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+    if(n<=2)
+        return n;
+        
+        int arr[n+1];
+         arr[0]=0;
+         arr[1]=1;
+         arr[2]=2;
+        for(int i=3;i<=n;i++)
+            arr[i]=arr[i-1]+arr[i-2];
+      return arr[n];
+    }
+};
