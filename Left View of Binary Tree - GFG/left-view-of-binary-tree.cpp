de6646f5284e463a037a4aafcf6f8d9013c15579
ARYAN SRIVASTAVA <90818029+11Aryan@users.noreply.github.com>
2023-06-13 23:26:42 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
@@ -128,24 +128,21 @@ struct Node
  */
 
 //Function to return a list containing elements of left view of the binary tree.
-void solve(Node*root,vector<int>&ans,int lvl)
-{
-    
-    if(root==NULL)
-      return;
-    
-    if(lvl==ans.size())
+void right(Node* root,vector<int>&ans,int level)
     {
-        ans.push_back(root->data);
+        if(!root) return;
+        if(level==ans.size())
+         {
+             ans.push_back(root->data);
+         }
+         right(root->left,ans,level+1);
+         right(root->right,ans,level+1);
+        
     }
-    solve(root->left,ans,lvl+1);
-    solve(root->right,ans,lvl+1);
-
-}
 vector<int> leftView(Node *root)
 {
    // Your code here
-   vector<int>ans;
-   solve(root,ans,0);
-   return ans;
+    vector<int>ans;
+     right(root,ans,0);
+        return ans;
 }
